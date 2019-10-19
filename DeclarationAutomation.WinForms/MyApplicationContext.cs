68c727c921b0c7aa31cc2f5a5da07ee6ab9c2380
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2019-10-19 17:31:21 +0200

    Initial commit

---
+++ DeclarationAutomation.WinForms/MyApplicationContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DeclarationAutomation.WinForms
+{
+    class NotificationService
+    {
+
+    }
+
+    class MyApplicationContext : ApplicationContext
+    {
+        private NotifyIcon notifyIcon;
+        private PreferencesForm preferencesForm;
+
+        public MyApplicationContext()
+        {
+            MenuItem configMenuItem = new MenuItem("Preferences", new EventHandler(ShowPerferences));
+            MenuItem exitMenuItem = new MenuItem("Exit", new EventHandler(ExitApplication));
+
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Icon = new Icon("appicon.ico");
+            notifyIcon.ContextMenu = new ContextMenu(new MenuItem[]
+                { configMenuItem, exitMenuItem });
+            notifyIcon.Visible = true;
+
+            preferencesForm = new PreferencesForm();
+        }
+
+        private void ShowPerferences(object sender, EventArgs e)
+        {
+            if(!preferencesForm.Visible)
+            {
+                preferencesForm.Show();
+            }
+        }
+
+        private void ExitApplication(object sender, EventArgs e)
+        {
+            // We must manually tidy up and remove the icon before we exit.
+            // Otherwise it will be left behind until the user mouses over.
+            notifyIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
---
+++ DeclarationAutomation.WinForms/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DeclarationAutomation.WinForms
+{
+    static class Program
+    {
+        static ServiceProvider serviceProvider;
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            SetupDependencyInjector();
+
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyApplicationContext());
+        }
+
+        private static void SetupDependencyInjector()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<NotificationService>();
+
+            serviceProvider = serviceCollection.BuildServiceProvider();
+
+            var notificationService = serviceProvider.GetService<NotificationService>();
+
+            Console.WriteLine(notificationService);
+        }
+    }
+}
