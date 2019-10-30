cea09a90317022dab7219c00f9f73f2055070529
Guido Arkesteijn <guidoarkesteijn@gmail.com>
2019-10-30 04:07:21 +0100

    Added error todos

--- DeclarationAutomation.Core/Report/IReport.cs
+++ DeclarationAutomation.Core/Report/IReport.cs
@@ -4,6 +4,8 @@ namespace DeclarationAutomation.Core.Report
 {
     public interface IReport
     {
+        //TODO Add Error class so we can log what went wrong.
+
         bool Success { get; }
         string ToString();
     }
--- DeclarationAutomation.Core/Sync/SyncError.cs
+++ DeclarationAutomation.Core/Sync/SyncError.cs
@@ -3,6 +3,7 @@ using DeclarationAutomation.Core.ErrorReporting;
 
 namespace DeclarationAutomation.Core.Sync
 {
+    //TODO use syncError to report back what is wrong in the report.
     public class SyncError : Error
     {
         public SyncError(string message) : base(message)
